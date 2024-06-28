4ff3117e330800450cb2aacaa4956d1583f407ce
Atharv Kulkarni <atkulkar@atkulkar-thinkpadp1gen3.punetw6.csb>
2024-06-28 11:57:17 +0530

    Got files retracked from stash

---
+++ src/components/redux_api/actions/productActions.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import * as actionType from '../constants/productConstants'
+
+const URL = 'https://rickandmortyapi.com/api/character'
+
+export const getProducts = ()=>async(dispatch)=>{
+    try {
+        const {data} = await axios.get(`${URL}`);
+        dispatch({type:actionType.GET_PRODUCTS_SUCCESS, payload:data})
+    } catch (error) {
+        dispatch({type:actionType.GET_PRODUCTS_FAIL, payload:error.message})
+    }
+}
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from './App';
 import '@patternfly/react-core/dist/styles/base.css';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './components/redux_api/store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
